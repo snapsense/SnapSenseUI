97729cd5bc5e8b2ffab8194f2789758fca8d3c1a
Alex Hall <me@jalexhall.com>
2015-03-02 19:29:35 -0500

    Initial UI

---
+++ WebContent/js/webaudioapi.js
@@ -0,0 +1,14 @@
+function bootstrapSpeechRecognition() {
+	recognition = new webkitSpeechRecognition();
+	button = document.getElementById("button");
+	recognition.continuous = true;
+	reset();
+	recognition.onend = reset;
+	recognition.onresult = function(event) {
+		for (var i = 0; i < event.results.length; ++i) {
+			if (event.results[i].isFinal === true) {
+				textarea.value += event.results[i][0].transcript;
+			}
+		}
+	};
+}
